882bfed7b0f99375b29ee39965576b5ef1ff64ab
Durvesh Kumar <durvesh88999@gamil.com>
2026-09-16 02:46:22 +0530

    update product class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,15 +10,15 @@ public class Main {
           double price;
           int storeId;
 
-          // Constructor of product class
-          public Product(String name, String category, String description, double price, int storeId){
-              this.productId = count++;
-              this.name = name;
-              this.category=category;
-              this.description=description;
-              this.price=price;
-              this.storeId=storeId;
-          }
+//          // Constructor of product class
+//          public Product(String name, String category, String description, double price, int storeId){
+//              this.productId = count++;
+//              this.name = name;
+//              this.category=category;
+//              this.description=description;
+//              this.price=price;
+//              this.storeId=storeId;
+//          }
 
           public int product(int x, int y){
               return x*y;
@@ -34,16 +34,18 @@ public class Main {
 
     // main method of main class
     public static void main(String[] args) {
-        Product product1 = new Product(
-                "Samsung Galaxy S24",
-                "Smartphone",
-                "128GB Storage, 8GB RAM, 50MP camara",
-                59999.00,
-                203
-        );
+//        Product p = new Product(
+//                "Samsung Galaxy S24",
+//                "Smartphone",
+//                "128GB Storage, 8GB RAM, 50MP camara",
+//                59999.00,
+//                203
+//        );
 
-        System.out.println("product x,y int: "+product1.product(3,6));
-        System.out.println("product x,y,z: "+ product1.product(5,2,7));
-        System.out.println("product x,y double: "+ product1.product(100.00, 50.00));
+        Product p = new Product();
+
+        System.out.println(p.product(3,6));
+        System.out.println(p.product(5,2,7));
+        System.out.println(p.product(100.00, 50.00));
     }
 }
