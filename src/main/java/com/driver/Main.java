77984e0badd768911fea155cf98055a4abb5d2f4
Durvesh Kumar <durvesh88999@gamil.com>
2026-09-16 03:00:45 +0530

    update code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,24 @@ package com.driver;
 
 public class Main {
       public static class Product {
+          private static int count=101;
+          int productId;
+          String name;
+          String category;
+          String description;
+          double price;
+          int storeId;
+
+          // Constructor of product class
+          public Product(String name, String category, String description, double price, int storeId){
+              this.productId = count++;
+              this.name = name;
+              this.category=category;
+              this.description=description;
+              this.price=price;
+              this.storeId=storeId;
+          }
+
           public int product(int x, int y){
               return x*y;
           }
@@ -13,8 +31,17 @@ public class Main {
               return  x*y;
           }
       }
+
+    // main method of main class
     public static void main(String[] args) {
-        Product p = new Product();
+        Product p = new Product(
+                "Samsung Galaxy S24",
+                "Smartphone",
+                "128GB Storage, 8GB RAM, 50MP camara",
+                59999.00,
+                203
+        );
+
         System.out.println(p.product(3,6));
         System.out.println(p.product(5,2,7));
         System.out.println(p.product(100.00, 50.00));
